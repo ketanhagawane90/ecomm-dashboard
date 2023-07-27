78fc48d1aa83972ac9571799ea2472a9e04993af
ketanhagawane90 <ketan.hagawane@gmail.com>
2023-07-27 19:16:10 +0530

    Navbar with page links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Header from './Header'
+import Login from './Login'
+import Register from './Register'
+import AddProduct from './AddProduct'
+import UpdateProduct from './UpdateProduct'
 
 function App() {
   return (
     <div className="App">
-      <h1>Ecomm Project</h1>
-      <button>Normal Button</button>
-      <Button>Bootstrap Button</Button>
+      <BrowserRouter>
+        <Header />
+        <h1>Ecomm Project</h1>
+        <Routes>
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+          <Route path="/add" Component={AddProduct} />
+          <Route path="/update" Component={UpdateProduct} />
+        </Routes>
+
+      </BrowserRouter>
+
+
+      {/*<button>Normal Button</button>
+      <Button>Bootstrap Button</Button>*/}
     </div>
   );
 }
